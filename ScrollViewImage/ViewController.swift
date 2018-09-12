55a4b4137a721ecf2f2419cffc9c865586eff386
Son <>
2018-09-12 19:53:19 +0700

    no message

--- ScrollViewImage/ViewController.swift
+++ ScrollViewImage/ViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollview: UIScrollView!
+    @IBOutlet weak var imageView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView!
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
